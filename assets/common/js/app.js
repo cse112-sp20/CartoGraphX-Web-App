352e96e90e380cf5ce24045f8937c1502c6d4e69
oscarvik <oscarcv@stud.ntnu.no>
2020-06-04 19:44:33 -0700

    feat: Webapp UI

--- assets/common/js/app.js
+++ assets/common/js/app.js
@@ -13,29 +13,28 @@ const getUiConfig = () => {
           if (authResult.user) {
             handleSignedInUser(authResult.user);
           }
-          if (authResult.additionalUserInfo) {
-            document.getElementById('is-new-user').textContent =
-                authResult.additionalUserInfo.isNewUser ?
-                'New User' : 'Existing User';
+          if (authResult.additionalUserInfo.isNewUser) {
+            
+            document.getElementById('is-new-user').textContent ='Welcome to CartoGraphx !';
+            document.getElementById('new-user-link').innerHTML = `<a href="https://marketplace.visualstudio.com/items?itemName=remote13.extension" target="_blank">VS Code extension</a>`
+            document.getElementById('new-user-info').style.display = 'flex'
           }
           // Do not redirect.
           return false;
         }
       },
       'signInOptions': [
-        // TODO(developer): Remove the providers you don't need for your app.
-        firebase.auth.GithubAuthProvider.PROVIDER_ID,
         {
           provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
           // Whether the display name should be displayed in Sign Up page.
           requireDisplayName: true,
-          signInMethod: 'emailLink'
         }
       ],
-      // Terms of service url.
+      /* Terms of service url.
       'tosUrl': 'https://www.google.com',
       // Privacy policy url.
       'privacyPolicyUrl': 'https://www.google.com',
+      */
       'credentialHelper': CLIENT_ID && CLIENT_ID != 'YOUR_OAUTH_CLIENT_ID' ?
           firebaseui.auth.CredentialHelper.GOOGLE_YOLO :
           firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM
@@ -45,7 +44,7 @@ const getUiConfig = () => {
   // Initialize the FirebaseUI Widget using Firebase.
   var ui = new firebaseui.auth.AuthUI(firebase.auth());
   // Disable auto-sign in.
-  ui.disableAutoSignIn();
+  //ui.disableAutoSignIn();
   
   
   
@@ -59,21 +58,13 @@ const getUiConfig = () => {
     document.getElementById('name').textContent = user.displayName;
     document.getElementById('email').textContent = user.email;
     document.getElementById('phone').textContent = user.phoneNumber;
-    if (user.photoURL) {
-      var photoURL = user.photoURL;
-      // Append size to the photo URL for Google hosted images to avoid requesting
-      // the image with its original resolution (using more bandwidth than needed)
-      // when it is going to be presented in smaller size.
-      if ((photoURL.indexOf('googleusercontent.com') != -1) ||
-          (photoURL.indexOf('ggpht.com') != -1)) {
-        photoURL = photoURL + '?sz=' +
-            document.getElementById('photo').clientHeight;
-      }
-      document.getElementById('photo').src = photoURL;
-      document.getElementById('photo').style.display = 'block';
-    } else {
-      document.getElementById('photo').style.display = 'none';
-    }
+    // update header
+    document.getElementById('userPage').textContent = "Profile";
+    document.getElementById('mapManagePage').textContent = "Manage Maps";
+
+    //load dummy tree
+    createTree("-M8S1dRRYzEbFIZBMaiR");
+  
   };
   
   
@@ -83,6 +74,9 @@ const getUiConfig = () => {
   var handleSignedOutUser = () => {
     document.getElementById('user-signed-in').style.display = 'none';
     document.getElementById('user-signed-out').style.display = 'block';
+    // update header
+    document.getElementById('userPage').innerHTML = null;
+    document.getElementById('mapManagePage').innerHTML = null;
     ui.start('#firebaseui-container', getUiConfig());
   };
   
@@ -98,10 +92,15 @@ const getUiConfig = () => {
    * Deletes the user's account.
    */
   var deleteAccount = () => {
-    firebase.auth().currentUser.delete().catch(function(error) {
+    firebase.auth().currentUser.delete().then( res =>{
+      console.log(res)
+      setTimeout(() => {
+        alert('Your account was successfully deleted.');
+      }, 1);
+    }).catch(error => {
       if (error.code == 'auth/requires-recent-login') {
         // The user's credential is too old. She needs to sign in again.
-        firebase.auth().signOut().then(function() {
+        firebase.auth().signOut().then(() => {
           // The timeout allows the message to be displayed after the UI has
           // changed to the signed out state.
           setTimeout(() => {
@@ -112,6 +111,15 @@ const getUiConfig = () => {
     });
   };
   
+  /** 
+   * Update navbar icon and collapsed menu visibility
+  */
+  const toggleNav = () => { 
+    document.querySelector('.burger i').classList.toggle('fa-bars');
+    document.querySelector('.burger i').classList.toggle('fa-times');
+    document.querySelector('.nav').classList.toggle('nav-active');
+  }
+
 
   
   /**
@@ -126,13 +134,57 @@ const getUiConfig = () => {
         'click', () => {
           deleteAccount();
     });
+
     document.addEventListener('submit', (e) => {
       e.preventDefault();
       const getMapForm = document.querySelector('#mapForm');
       let mapK = getMapForm['mapKey'].value;
-      console.log(mapK);
       createTree(mapK);
-  });
+    });
+    // display firebaseUI on button click
+    document.getElementById('large-sign-button').addEventListener('click', () => {
+      document.getElementById('large-sign-button').style.display = 'none';
+      document.getElementById('firebaseui-spa').style.display = 'block';
+    });
+
+    //navigation bar listeners
+    document.querySelector('.burger i').addEventListener('click', () => {
+      toggleNav();
+    });
+    // close profile on click outside
+    window.onclick = (e) => {
+      const userProfile = document.getElementById('user-profile');
+      const userProfileButton = document.getElementById('userPage')
+      if (e.target !== userProfile && !userProfile.contains(e.target) && e.target !== userProfileButton) {
+        document.getElementById('user-profile').style.display = 'none';
+      }
+    }
+
+    document.getElementById('userPage').addEventListener('click', (e) => {
+      document.getElementById('mapmanage-container').style.display = 'none';
+      if(document.getElementById('user-profile').style.display !== 'block'){
+        document.getElementById('user-profile').style.display = 'block';
+      }else{
+        document.getElementById('user-profile').style.display = 'none';
+      }
+      toggleNav();
+    });
+
+    document.getElementById('mapManagePage').addEventListener('click', () => {
+      document.getElementById('user-profile').style.display = 'none';
+      if(document.getElementById('mapmanage-container').style.display !== 'flex'){
+        document.getElementById('mapmanage-container').style.display = 'flex';
+      }else{
+        document.getElementById('mapmanage-container').style.display = 'none';
+      }
+      toggleNav();
+    });
+
+    // Manage Map module listener
+    document.querySelector('#manage-close-button i').addEventListener('click', () => {
+      document.getElementById('mapmanage-container').style.display = 'none';
+    });
+
   };
   
   window.addEventListener('load', initApp);
