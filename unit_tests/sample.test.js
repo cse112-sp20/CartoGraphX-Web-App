4c223bcc19bf44b390da6a0e68d408ee7cacbe9a
Shardul Saiya <sssaiya@ucsd.edu>
2020-06-15 21:05:13 -0700

    Breaking changes
    Sorry about that !

--- unit_tests/sample.test.js
+++ unit_tests/sample.test.js
@@ -2,6 +2,7 @@ var assert = require("assert");
 describe("Array", () => {
   describe("#indexOf()", () => {
     it("should return -1 when the value is not present", () => {
+      assert.equal(2,3);
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
